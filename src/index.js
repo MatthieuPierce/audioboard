ae858ecba83449d3cf55685257410a4b7f2781d2
Matthieu Pierce <matthieu.pierce@gmail.com>
2021-02-01 17:52:52 -0800

    start on root reducer, establish keypads

--- src/index.js
+++ src/index.js
@@ -4,9 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import rootReducer from './redux/rootReducer.js'
+import store from './redux/store' ;
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    < Provider store={ store } >
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
